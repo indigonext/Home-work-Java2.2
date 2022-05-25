da51b1f26d0b5a0f0e473665fb6143243def4b9c
indigonext <novikovandrey7177@gmail.com>
2022-05-25 17:30:44 +0300

    2 доработка

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
     int score = 100; // сумма которая была на счету
-    int deposit = 1000; // сумма которую вносят на счет
+    int deposit = 1100; // сумма которую вносят на счет
     if (deposit > 1000){
-        double bonus = (deposit * 0.01);
+        int percent = 1;
+        int bonus = (deposit / 100 * percent);
         System.out.println ("Бонус за пополнение " + bonus + " рублей");
-        double depositBonus = (deposit + bonus);
+        int depositBonus = (deposit + bonus);
         System.out.println ("Сумма пополнения с бонусом " + depositBonus + " рублей");
-        double finalScore = (score + depositBonus);
+        int finalScore = (score + depositBonus);
         System.out.println ("Сумма на счете " + finalScore + " рублей");
     } else {
         System.out.println ("За пополнение от 1000 рублей можно получить бонус на счет");
